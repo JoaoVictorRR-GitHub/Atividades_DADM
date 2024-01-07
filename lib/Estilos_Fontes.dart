98ca06e124b7afdd3f482dcf6a38881cd6d07e26
JoaoVictorRR-GitHub <joaovictor.rr@hotmail.com>
2024-01-07 23:49:00 +0000

    Estilos e fontes.

---
+++ lib/Estilos_Fontes.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+// Estilos e fontes padroes.
+const TextStyle styleTitulo = TextStyle(fontWeight: FontWeight.bold, color: Colors.lightBlueAccent);
+const TextStyle styleBotao  = TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Colors.white70);
